2cf1b25ba860badbe92cbd9a84ccdb1f44bd9281
John Colagioia <john@colagioia.net>
2021-11-21 06:53:16 -0500

    Handle network communication to INTERN

--- lib/ask_intern.ex
+++ lib/ask_intern.ex
@@ -4,16 +4,19 @@ defmodule AskIntern do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> AskIntern.hello()
-      :world
+  A simple network client to connect to INTERN, send a query string,
+  and print the response.
 
   """
   def main(args) do
     query = Enum.join(args, " ")
-    IO.puts query
+    opts = [active: false]
+    {:ok, socket} = :gen_tcp.connect('localhost', 48813, opts)
+    :ok = :gen_tcp.send(socket, query)
+    case :gen_tcp.recv(socket, 0, 10000) do
+      {:ok, response} -> IO.puts response
+      {:error, reason} -> IO.puts "#{reason}"
+    end
+    :ok = :gen_tcp.close(socket)
   end
 end
