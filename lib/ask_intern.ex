66a2afbcb69a1c30bee649e79773693c2216ebae
John Colagioia <john@colagioia.net>
2021-11-24 06:36:16 -0500

    Use port number from INTERN's configuration file

--- lib/ask_intern.ex
+++ lib/ask_intern.ex
@@ -9,9 +9,25 @@ defmodule AskIntern do
 
   """
   def main(args) do
+    #
+    # Get the port from INTERN's configuration file.
+    #
+    config_folder = :filename.basedir(:user_config, "intern")
+    config_file = Path.join(config_folder, "intern.json")
+    json = case File.read(config_file) do
+      {:ok, slug} -> slug
+      {:error, reason} ->
+        IO.puts "#{reason}"
+        "{\"server\":{\"port\":48813}}"
+    end
+    config = Poison.decode!(json)
+    port = config["server"]["port"]
+    #
+    # Send query to INTERN and print response.
+    #
     query = Enum.join(args, " ")
     opts = [active: false]
-    {:ok, socket} = :gen_tcp.connect('localhost', 48813, opts)
+    {:ok, socket} = :gen_tcp.connect('localhost', port, opts)
     :ok = :gen_tcp.send(socket, query)
     case :gen_tcp.recv(socket, 0, 10000) do
       {:ok, response} -> IO.puts response
