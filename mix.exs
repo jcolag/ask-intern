708b2b1866dc0fa88e426c6a43251f397bd6af2d
John Colagioia <john@colagioia.net>
2021-11-24 06:33:06 -0500

    Add "Poison" JSON library

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AskIntern.MixProject do
     [
       app: :ask,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.11",
       escript: [main_module: AskIntern],
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -22,6 +22,7 @@ defmodule AskIntern.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:poison, "~> 5.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
